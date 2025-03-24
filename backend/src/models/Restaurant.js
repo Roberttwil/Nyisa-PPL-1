96599dbf29d9385db8d726afcc85d881ad5b521c
Nurkahfi A Rahmada <110613182+nrkahfirhmd@users.noreply.github.com>
2025-03-25 01:14:48 +0700

    feat: add Restaurant model

---
+++ backend/src/models/Restaurant.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Restaurant = sequelize.define(
+    "Restaurant",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        restaurant_type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        photo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        longitude: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        latitude: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+    {
+        tableName: "restaurant",
+        timestamps: false
+    }
+)
+
+module.exports = Restaurant;    
